e00ec37cc4fbedbd6f4f21280128c871224c2651
jegors <jegors@jegors-debian.local>
2024-02-22 14:50:02 +0100

    Now working

--- sketch_feb22a/sketch_feb22a.ino
+++ sketch_feb22a/sketch_feb22a.ino
@@ -1,31 +1,40 @@
-// Motor 1
 int in1 = 7;
 int in2 = 8;
+int in3 = 12;
+int in4 = 13;
 
 void setup() {
-  // put your setup code here, to run once:
+
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
 
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
   spin();
 
 }
 
 void spin() {
 
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
 
   delay(2000);
 
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
 
 }
