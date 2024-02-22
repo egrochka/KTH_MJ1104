25cb43d51bff09fde6981a6cb3ffbf7671f85d35
egrochka <jegors@debian>
2024-02-22 03:43:45 +0100

    Basic motor (not working)

--- sketch_feb22a/sketch_feb22a.ino
+++ sketch_feb22a/sketch_feb22a.ino
@@ -1,9 +1,31 @@
+// Motor 1
+int in1 = 7;
+int in2 = 8;
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  spin();
+
+}
+
+void spin() {
+
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+
+  delay(2000);
+
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
 
 }
