40f6748106ac50e78751c569e69f9b35028a0465
egrochka <jegors@debian>
2024-04-08 11:50:06 +0200

    Added basic ir sensor functionality

--- sketch_feb22a/sketch_feb22a.ino
+++ sketch_feb22a/sketch_feb22a.ino
@@ -1,14 +1,26 @@
-int in1 = 7;
-int in2 = 8;
-int in3 = 12;
-int in4 = 13;
+const int in1 = 7;
+const int in2 = 8;
+const int in3 = 12;
+const int in4 = 13;
+
+const int ir1 = 2;
+const int ir2 = 4;
+
+int state = 0;
+
+int mode = false;
 
 void setup() {
+
+  Serial.begin(9600);
 
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
+
+  pinMode(ir1, INPUT)
+  pinMode(ir2, INPUT)
 
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
@@ -19,12 +31,31 @@ void setup() {
 
 void loop() {
 
-  //spin1();
+  if(!mode) {
+    spin1();
 
-  //delay(1000);
+    //delay(1000);
 
-  spin2();
+    //spin2();
 
+  } else {
+
+    if(ir1_input() && state != 1) {
+      state = 1;
+      spin2();
+      print_state()
+
+    } else if(ir_input2() && state != 2) {
+      spin1();
+      state = 2;
+
+    } else if(!ir_input1() && !ir_input2()) {
+      state = 0;
+    
+    }
+
+    delay(20);
+  }
 }
 
 void spin1() {
@@ -51,4 +82,16 @@ void spin2() {
   
 }
 
+int ir1_input() {
+  return !digitalRead(ir1);
 
+}
+
+int ir2_input() {
+  return !digitalRead(ir2);
+
+}
+
+void print_state() {
+  Serial.print(state);
+}
