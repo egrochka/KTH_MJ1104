d82a3ddfa2cabcce8cd3c8e7bf2afdade83cc044
egrochka <jegors@debian>
2024-04-14 17:08:06 +0200

    Sensors now working:

--- sketch_feb22a/sketch_feb22a.ino
+++ sketch_feb22a/sketch_feb22a.ino
@@ -8,7 +8,7 @@ const int ir2 = 4;
 
 int state = 0;
 
-int mode = false;
+int mode = 2;
 
 void setup() {
 
@@ -19,8 +19,8 @@ void setup() {
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
 
-  pinMode(ir1, INPUT)
-  pinMode(ir2, INPUT)
+  pinMode(ir1, INPUT);
+  pinMode(ir2, INPUT);
 
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
@@ -29,69 +29,67 @@ void setup() {
 
 }
 
-void loop() {
-
-  if(!mode) {
-    spin1();
-
-    //delay(1000);
-
-    //spin2();
-
-  } else {
-
-    if(ir1_input() && state != 1) {
-      state = 1;
-      spin2();
-      print_state()
-
-    } else if(ir_input2() && state != 2) {
-      spin1();
-      state = 2;
-
-    } else if(!ir_input1() && !ir_input2()) {
-      state = 0;
-    
-    }
-
-    delay(20);
-  }
-}
-
-void spin1() {
-
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
-
-  delay(2000);
+void spinLeft() {
 
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
 
 }
 
-void spin2() {
-
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH);
-
-  delay(2000);
+void spinRight() {
 
   digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW);
   
 }
 
-int ir1_input() {
+int irLeft_input() {
   return !digitalRead(ir1);
 
 }
 
-int ir2_input() {
+int irRight_input() {
   return !digitalRead(ir2);
 
 }
 
 void print_state() {
   Serial.print(state);
+}
+
+void loop() {
+
+  if(mode == 1) {
+    spinLeft();
+    spinRight();
+
+  } else if(mode == 2){
+
+    if(irLeft_input() && !irRight_input() && state != 1) {
+      state = 1;
+      digitalWrite(in3, LOW);
+      digitalWrite(in4, LOW);
+      print_state();
+      spinLeft();
+
+    } else if(irRight_input() && !irLeft_input() && state != 2) {
+      state = 2;
+      digitalWrite(in1, LOW);
+      digitalWrite(in2, LOW);
+      print_state();
+      spinRight
+    ();
+
+    } else if(irLeft_input() && irRight_input()) {
+      state = 0;
+      digitalWrite(in1, LOW);
+      digitalWrite(in2, LOW);
+      digitalWrite(in3, LOW);
+      digitalWrite(in4, LOW);
+      delay(2000);
+    
+    }
+
+    delay(200);
+  }
 }
